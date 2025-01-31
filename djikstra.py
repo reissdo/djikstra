c77d7068ff9f8fc85b4b09a30a9f970afff57a00
Dominik Reiss <d.reiss@gerhard-schubert.de>
2025-01-31 23:32:15 +0100

    finished djikstra

--- djikstra.py
+++ djikstra.py
@@ -1,37 +1,90 @@
 import numpy as np
-import math
 
-adjMat=         [[0,1,0,4,3,0],
-                 [1,0,5,0,1,0],
-                 [0,5,0,2,1,0],
-                 [4,0,2,0,2,0],
-                 [3,1,1,2,0,1],
-                 [0,0,1,0,0,0]]
-startNode = 1
+def createGraph(nNodes,maxWeight = 10,probab = 0.8):
+    """
+    +number of nodes [int]
+    
+    creates random adjacency matrix
+    
+    -adjacency matrix [numpy array]
+    """
+    
+    matrix = np.random.randint(1, maxWeight + 1, size=(nNodes, nNodes))
+    mask = np.random.rand(nNodes, nNodes) < probab
+    matrix = matrix * mask
+    np.fill_diagonal(matrix, 0)
 
-def djikstra(adjMat,startNode):
-    if(adjMat.shape[0] != adjMat.shape[1]):
+    return matrix
+
+def djikstra(adjMat,startNode,endNode):
+    """
+    +adjacency matrix [numpy array]
+    +start node [int]
+    +end node [int]
+    
+    finds the shortes path between start node and end node
+    
+    -path from start node to end node [list]
+    """
+    
+    if adjMat.shape[0] != adjMat.shape[1]:
         print("error: number of rows != number of cols")
         print(adjMat.shape)
         return
     
-    table = np.zeros((adjMat.shape[0],3))
+    if startNode >= adjMat.shape[0] or endNode >= adjMat.shape[0]:
+        print("error: start node or end node does not exist")
+        print("start node: ",startNode,"\nend node: ",endNode)
+        return
+    
+    table = np.zeros((adjMat.shape[0],3),dtype=int) #list for every node: [wasVisited,cost,previousNode]
     
     for row in table:
-        row[1] = math.inf
+        row[1] = 1000
         row[2] = -1
 
     table[startNode][1] = 0 
     
+    #start djikstra
     for i in range(adjMat.shape[0]):
         
+        notVisited = []
+        for i in range(table.shape[0]):
+            if table[i][0] == 0:
+                notVisited.append([i,table[i][1]])
+        
+        currentNode = min(notVisited,key=lambda n: n[1])[0]
+        table[currentNode][0] = 1
+        
+        for j in range(adjMat[currentNode].shape[0]):
+            if adjMat[currentNode][j] == 0 or table[j][0] == 1:
+                continue
+            else:
+                cost = table[currentNode][1] + adjMat[currentNode][j]
+                if cost < table[j][1]:
+                    table[j][1] = cost
+                    table[j][2] = currentNode
+                         
+    path = []
+    currentNode = endNode
+    path.append(currentNode)
     
-    print(table)
+    while currentNode != startNode:
+        if table[currentNode][2] == -1:
+            print("path from start node to end node is not possible")
+            return
+        else:
+            nextNode = table[currentNode][2]
+            path.append(nextNode)
+            currentNode = nextNode
+    path.reverse()
+    return path
 
 
-    return
+adjMat = createGraph(20,10,0.1)
+print(adjMat)
+startNode = 0
+endNode = 19
 
-
-
-
-djikstra(np.asarray(adjMat),startNode)
+path = djikstra(adjMat,startNode,endNode)
+print(path)
